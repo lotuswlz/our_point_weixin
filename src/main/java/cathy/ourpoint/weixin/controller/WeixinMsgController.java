b7eb7608755b222cc26f699d9c223835df1cb255
WuLingzi <lotuswlz@sina.com>
2015-06-17 23:03:20 +0800

    fix utf-8 issues

--- src/main/java/cathy/ourpoint/weixin/controller/WeixinMsgController.java
+++ src/main/java/cathy/ourpoint/weixin/controller/WeixinMsgController.java
@@ -14,44 +14,22 @@ import cathy.jfinal.weixin.sdk.msg.in.speech_recognition.InSpeechRecognitionResu
 import cathy.jfinal.weixin.sdk.msg.out.*;
 import com.jfinal.kit.PropKit;
 
-/**
- * 将此 DemoController 在YourJFinalConfig 中注册路由，
- * 并设置好weixin开发者中心的 URL 与 token ，使 URL 指向该
- * DemoController 继承自父类 WeixinController 的 index
- * 方法即可直接运行看效果，在此基础之上修改相关的方法即可进行实际项目开发
- */
 public class WeixinMsgController extends MsgController {
 	
 	private static final String helpStr = "\t发送 help 可获得帮助，发送 \"美女\" 可看美女，发送 news 可看新闻，发送 music 可听音乐，你还可以试试发送图片、语音、位置、收藏等信息，看会有什么 。公众号持续更新中，想要更多惊喜欢迎每天关注 ^_^";
 	
-	/**
-	 * 如果要支持多公众账号，只需要在此返回各个公众号对应的  ApiConfig 对象即可
-	 * 可以通过在请求 url 中挂参数来动态从数据库中获取 ApiConfig 属性值
-	 */
 	public ApiConfig getApiConfig() {
 		ApiConfig ac = new ApiConfig();
 		
-		// 配置微信 API 相关常量
 		ac.setToken(PropKit.get("token"));
 		ac.setAppId(PropKit.get("appId"));
 		ac.setAppSecret(PropKit.get("appSecret"));
 		
-		/**
-		 *  是否对消息进行加密，对应于微信平台的消息加解密方式：
-		 *  1：true进行加密且必须配置 encodingAesKey
-		 *  2：false采用明文模式，同时也支持混合模式
-		 */
 		ac.setEncryptMessage(PropKit.getBoolean("encryptMessage", false));
 		ac.setEncodingAesKey(PropKit.get("encodingAesKey", "setting it in config file"));
 		return ac;
 	}
 	
-	/**
-	 * 实现父类抽方法，处理文本消息
-	 * 本例子中根据消息中的不同文本内容分别做出了不同的响应，同时也是为了测试 jfinal weixin sdk的基本功能：
-	 *     本方法仅测试了 OutTextMsg、OutNewsMsg、OutMusicMsg 三种类型的OutMsg，
-	 *     其它类型的消息会在随后的方法中进行测试
-	 */
 	protected void processInTextMsg(InTextMsg inTextMsg) {
 		String msgContent = inTextMsg.getContent().trim();
 		// 帮助提示
